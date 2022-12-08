a63ad1c76787fec6feba861b9ae2b229e6d53851
Jason <74917583+Json-93@users.noreply.github.com>
2022-12-09 01:06:23 +0800

    Update natral_sampling.py

--- natral_sampling.py
+++ natral_sampling.py
@@ -2,26 +2,37 @@ import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
 
-N = 1024
+N = 512
 t = np.linspace(-1, 1, N)
 
 # def rect(x):
 #     return np.where(np.abs(x)<=0.5, 1, -1)
 
 x1 = np.sin(2*np.pi*1*t)
-x2 = 0.5 * (signal.square(50*np.pi*1*t) + 1)
+x2 = 0.5 * (signal.square(2*np.pi*1*t) + 1)
 
 y1 = x1*x2
 
-plt.subplot(411), plt.plot(t, x1, 'blue')
-plt.grid(True)
-plt.subplot(412), plt.plot(t, x2, 'blue')
-plt.grid(True)
-plt.subplot(413), plt.plot(t, y1, 'blue')
+plt.subplot(221), plt.plot(t, x1, 'blue')
+plt.xlabel('time')
+plt.title('sin wave')
 plt.grid(True)
 
+plt.subplot(222), plt.plot(t, x2, 'blue')
+plt.grid(True)
+plt.xlabel('time')
+plt.title('square wave')
 
+plt.subplot(223), plt.plot(t, y1, 'blue')
+plt.ylim(-1.1, 1.1)
+plt.grid(True)
+plt.xlabel('time')
+plt.title('natral sampling')
+
+fs = 10
 n = np.arange(-1, 1, 1/N)
-y1_fft = np.abs(np.fft.fft(np.sin(2*np.pi*1*n) * 0.5 * (signal.square(50*np.pi*1*n) + 1)))
-plt.subplot(414), plt.plot(n, y1_fft, 'blue')
+y1_fft = np.abs(np.fft.fft(np.sin(2*np.pi*1*n) * 0.5 * (signal.square(fs*np.pi*n) + 1)))
+plt.subplot(224), plt.plot(n, y1_fft, 'blue')
+plt.xlabel('frequence')
+plt.title('freq of natral sampling')
 plt.show()
