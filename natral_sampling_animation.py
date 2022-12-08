cf563a2354be8d99ce045babfb2237b8ff09ad1d
Jason <74917583+Json-93@users.noreply.github.com>
2022-12-09 01:06:42 +0800

    Update natral_sampling_animation.py

--- natral_sampling_animation.py
+++ natral_sampling_animation.py
@@ -4,35 +4,40 @@ import matplotlib.pyplot as plt
 
 # N = 1024
 plt.ion()
-for N in range(8, 4096, 16):
+for fs in range(1, 100, 1):
+    N = 512
     t = np.linspace(-1, 1, N)
 
     # def rect(x):
     #     return np.where(np.abs(x)<=0.5, 1, -1)
 
     x1 = np.sin(2*np.pi*1*t)
-    x2 = 0.5 * (signal.square(50*np.pi*1*t) + 1)
+    x2 = 0.5 * (signal.square(fs*np.pi*t) + 1)
 
     y1 = x1*x2
 
-    plt.subplot(411), plt.plot(t, x1, 'blue')
-    plt.title(f'N = {N}')
+    plt.subplot(221), plt.plot(t, x1, 'blue')
+    plt.xlabel('time')
+    plt.title('sin wave')
     plt.grid(True)
-    plt.ylabel('sin(2 pi f t)')
-    
-    plt.subplot(412), plt.plot(t, x2, 'blue')
-    plt.grid(True)
-    plt.ylabel('square wave')
 
-    plt.subplot(413), plt.plot(t, y1, 'blue')
+    plt.subplot(222), plt.plot(t, x2, 'blue')
     plt.grid(True)
-    plt.ylabel('natral sampling')
+    plt.xlabel('time')
+    plt.title('square wave')
 
+    plt.subplot(223), plt.plot(t, y1, 'blue')
+    plt.ylim(-1.1, 1.1)
+    plt.grid(True)
+    plt.xlabel('time')
+    plt.title('natral sampling')
 
     n = np.arange(-1, 1, 1/N)
-    y1_fft = np.abs(np.fft.fft(np.sin(2*np.pi*1*n) * 0.5 * (signal.square(50*np.pi*1*n) + 1)))
-    plt.subplot(414), plt.plot(n, y1_fft, 'blue')
+    y1_fft = np.abs(np.fft.fft(np.sin(2*np.pi*1*n) * 0.5 * (signal.square(fs*np.pi*n) + 1)))
+    plt.subplot(224), plt.plot(n, y1_fft, 'blue')
+    plt.xlabel('frequence')
+    plt.title('freq of natral sampling')
     
     plt.show()
-    plt.pause(0.005)
+    plt.pause(0.5)
     plt.clf()
